e4762553de4d8a6840609d9416f4867202e10909
zulu0808 <pvolcy1@gmail.com>
2020-11-10 12:35:00 -0600

    nymex ftp

---
+++ 05-APIs/census_q12018.sql
@@ -0,0 +1,14 @@
+CREATE TABLE census_q12018 (
+	Geography VARCHAR (255)
+	, Quarterly_Establishments VARCHAR(255)
+	, July_Employment VARCHAR (255)
+	, August_Employment	VARCHAR (255)
+	, September_Employment VARCHAR (255)
+	, Total_Quarterly_Wages	VARCHAR (255)
+	, Average_Weekly_Wage VARCHAR (255)
+	, September_Employment_Location_Quotient INT
+	, Total_Quarterly_Wages_Location_Quotient INT
+	
+);
+
+SELECT * FROM census_q12018;
